042b1ddea9ab898751604c40e15ae8c6c68d90e1
chrystian <chrystian_sales@hotmail.com>
2022-11-25 20:25:49 -0300

    primeiro commit

---
+++ js/firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA6IqlPSmPnuH1DC3_PcTm5Gbq8Ou2TwCI",
+  authDomain: "vetclin2-38171.firebaseapp.com",
+  projectId: "vetclin2-38171",
+  storageBucket: "vetclin2-38171.appspot.com",
+  messagingSenderId: "460392709212",
+  appId: "1:460392709212:web:39aaaf7a06f3ccb542ba99"
+};
+
+// Inicializando o Firebase
+const app = initializeApp(firebaseConfig);
+var database = firebase.database();
